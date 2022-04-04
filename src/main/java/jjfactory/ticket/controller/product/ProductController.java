0b447ab7c282f495b4b1c82b26ebd787ca4aff0e
jae <wogud1514@naver.com>
2022-04-04 21:59:36 +0900

    css 적용추가

--- src/main/java/jjfactory/ticket/controller/product/ProductController.java
+++ src/main/java/jjfactory/ticket/controller/product/ProductController.java
@@ -1,14 +1,26 @@
 package jjfactory.ticket.controller.product;
 
+import jjfactory.ticket.service.product.ProductServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class ProductController {
 
+    private final ProductServiceImpl productService;
+
+    @GetMapping("product")
+    public String getProducts(Model model){
+        model.addAttribute("products",productService.getProductList());
+        return "product/products";
+    }
+
     @GetMapping("product/{id}")
-    public String getProduct(@PathVariable Long id){
-        return "productDetail";
+    public String getProductDetail(@PathVariable Long id){
+        return "product/productDetail";
     }
 }
